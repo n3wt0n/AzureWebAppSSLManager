df78a07a794bef8bfc7c4bfaded4c4b083b77473
thecontrarycat <1482576+thecontrarycat@users.noreply.github.com>
2020-03-30 17:30:21 +0100

    Add comments and examples for setting the AppServicePlan's ResourceGroup

--- src/WebAppSSLManager/Models/AppProperty.cs
+++ src/WebAppSSLManager/Models/AppProperty.cs
@@ -8,7 +8,7 @@ namespace WebAppSSLManager.Models
         public string Hostname { get; set; }
         public string ResourceName { get; set; } //either the WebApp name or the FunctionApp name
         public string ResourceResGroup { get; set; } //the resource group container the WebApp or the FunctionApp
-        public string ResourcePlanResGroup { get; set; }
+        public string ResourcePlanResGroup { get; set; } //(optional) If the AppServicePlan is in a different resource group, this is the name of the Plan's resource group
         public string AzureDnsZoneName { get; set; }
         public string AzureDnsResGroup { get; set; }
 
@@ -25,7 +25,7 @@ namespace WebAppSSLManager.Models
 
         public string BaseDomain => AzureDnsZoneName;
   
-        public string PlanResourceGroup => ResourcePlanResGroup ?? ResourceResGroup;
+        public string PlanResourceGroup => ResourcePlanResGroup ?? ResourceResGroup; // Use the resource group of the AppService or FunctionApp if it has not explictly been set for the AppServicePlan
         
         public string PfxFileName
             => $"{HostnameFriendly}.pfx";
