0494bdd1752b0c8525645367c885651cb99edd84
Kirsty <github@thecontrarycat.com>
2020-03-31 02:24:02 +0100

    App Service Plan can be in a different ResourceGroup to the App Service (#20)
    * Allow the user to configure the resource group of the app service plan so that certificates can be uploaded to it and assigned to app services in different resource groups
    
    * Add comments and examples for setting the AppServicePlan's ResourceGroup
    
    Co-authored-by: thecontrarycat <1482576+thecontrarycat@users.noreply.github.com>

--- src/WebAppSSLManager/Models/AppProperty.cs
+++ src/WebAppSSLManager/Models/AppProperty.cs
@@ -8,6 +8,7 @@ namespace WebAppSSLManager.Models
         public string Hostname { get; set; }
         public string ResourceName { get; set; } //either the WebApp name or the FunctionApp name
         public string ResourceResGroup { get; set; } //the resource group container the WebApp or the FunctionApp
+        public string ResourcePlanResGroup { get; set; } //(optional) If the AppServicePlan is in a different resource group, this is the name of the Plan's resource group
         public string AzureDnsZoneName { get; set; }
         public string AzureDnsResGroup { get; set; }
 
@@ -24,6 +25,8 @@ namespace WebAppSSLManager.Models
 
         public string BaseDomain => AzureDnsZoneName;
   
+        public string PlanResourceGroup => ResourcePlanResGroup ?? ResourceResGroup; // Use the resource group of the AppService or FunctionApp if it has not explictly been set for the AppServicePlan
+        
         public string PfxFileName
             => $"{HostnameFriendly}.pfx";
 
